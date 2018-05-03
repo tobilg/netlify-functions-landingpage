c672fef0ec8eafa952121b2fd4c4ad1480e27e52
TobiLG <tobilg@gmail.com>
2018-05-03 08:49:46 +0200

    Initial upload

---
+++ src/App.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import './bootstrap.css'
+import './App.css'
+
+const formAPI = '/.netlify/functions/signup'
+
+export default class App extends Component {
+    state = {
+        loading: false,
+        success: false,
+        error: false
+    }
+    handleSubmit = (event, data) => {
+        event.preventDefault()
+        const email = this.email.value
+
+        if (!email) {
+            alert('Please email your email')
+        }
+
+        this.setState({
+            loading: true
+        })
+
+        formHandler(email).then(() => {
+            this.setState({
+                success: true,
+                loading: false
+            })
+        }).catch((e) => {
+            this.setState({
+                error: true,
+                loading: false
+            })
+        })
+    }
+    renderForm() {
+        const { success, loading } = this.state
+        const buttonText = (loading) ? '...' : 'Notify Me'
+        const handler = (loading) ? noOp : this.handleSubmit
+
+        /* if they submitted the form, show thanks */
+        if (success) {
+            return (
+                <div>
+                <h2>Thanks for signing up!</h2>
+            </div>
+        )
+        }
+
+        return (
+            <form onSubmit={handler}>
+            <input
+        type="email"
+        name="email"
+        className="sign-up"
+        ref={input => this.email = input}
+        placeholder="Enter your email address..."
+        required
+        />
+        <button className="btn btn-lg btn-green sign-up-button" type="submit">
+            {buttonText}
+            </button>
+            </form>
+    )
+    }
+    render() {
+        return (
+            <div className="App">
+            <div className="landing-page">
+            <div className='backdrop'></div>
+            <div className='container'>
+            <div className="row centered">
+            <h3 className="logo aligncenter">
+            Serverless Components
+        </h3>
+
+        <h1>
+        Sign up to get notified<br/>when we launch!
+        </h1>
+
+        <div className="col-md-6 col-md-offset-3 mt">
+            {this.renderForm()}
+    </div>
+        </div>
+
+        <div className="row">
+            <div className="continue">
+            <i className="ion-chevron-down"></i>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+    )
+    }
+}
+
+function formHandler(email) {
+    const data = {
+        email: email
+    }
+    return axios({
+        method: 'post',
+        url: formAPI,
+        data: data,
+    })
+}
+
+function noOp() {
+    console.log('submission in progress')
+}
---
+++ src/lambda/signup.js
@@ -0,0 +1,77 @@
+const request = require("request");
+
+const mailChimpAPI = process.env.MAILCHIMP_API_KEY;
+const mailChimpListID = process.env.MAILCHIMP_LIST_ID;
+const mcRegion = process.env.MAILCHIMP_REGION;
+
+module.exports.handler = (event, context, callback) => {
+
+    const formData = JSON.parse(event.body);
+    const email = formData.email;
+    let errorMessage = null;
+
+    if (!formData) {
+        errorMessage = "No form data supplied";
+        console.log(errorMessage);
+        callback(errorMessage);
+    }
+
+    if (!email) {
+        errorMessage = "No EMAIL supplied";
+        console.log(errorMessage);
+        callback(errorMessage);
+    }
+
+    if (!mailChimpListID) {
+        errorMessage = "No LIST_ID supplied";
+        console.log(errorMessage);
+        callback(errorMessage);
+    }
+
+    const data = {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {}
+    };
+
+    const subscriber = JSON.stringify(data);
+    console.log("Sending data to mailchimp", subscriber);
+
+    request({
+        method: "POST",
+        url: `https://${mcRegion}.api.mailchimp.com/3.0/lists/${mailChimpListID}/members`,
+        body: subscriber,
+        headers: {
+            "Authorization": `apikey ${mailChimpAPI}`,
+            "Content-Type": "application/json"
+        }
+    }, (error, response, body) => {
+        if (error) {
+            callback(error, null)
+        }
+        const bodyObj = JSON.parse(body);
+
+        console.log("Mailchimp body: " + JSON.stringify(bodyObj));
+        console.log("Status Code: " + response.statusCode);
+
+        if (response.statusCode < 300 || (bodyObj.status === 400 && bodyObj.title === "Member Exists")) {
+            console.log("Added to list in Mailchimp subscriber list");
+            callback(null, {
+                statusCode: 201,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Credentials": "true"
+                },
+                body: JSON.stringify({
+                    status: "saved email"
+                })
+            })
+        } else {
+            console.log("Error from mailchimp", bodyObj.detail);
+            callback(bodyObj.detail, null);
+        }
+
+    });
+    
+};
